3cba29cb394a0eb3c7b865f969add34cf3183712
mehesultana <sultanamehe01@gmail.com>
2021-10-19 19:48:32 +0600

    banner update

--- src/pages/Banner/Banner.js
+++ src/pages/Banner/Banner.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Card, CardGroup, Container } from 'react-bootstrap';
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
+import { FcSearch, FcNightPortrait } from 'react-icons/fc';
+import { Link } from 'react-router-dom';
 import './Banner.css';
 
 const Banner = () => {
@@ -8,32 +10,22 @@ const Banner = () => {
 			<div className="banner-image">
 				<h1 className="text-center">Welcome to My Home Care</h1>
 				<h1 className="text-center">Our Commitment to Safe Care</h1>
-				<CardGroup className="advantage">
-					<Card className="card">
-						<Card.Body>
-							<Card.Title>0ur Doctors</Card.Title>
 
-							<Card.Text>Choose by name, specialty, city and more</Card.Text>
-
-							<button>Find Doctor</button>
-						</Card.Body>
-					</Card>
-					<Card className="card">
-						<Card.Body>
-							<Card.Title>Locations & Directions</Card.Title>
-							<Card.Text>Find maps and more for all locations</Card.Text>
-							<button>Find a Location</button>
-						</Card.Body>
-					</Card>
-					<Card className="card">
-						<Card.Body>
-							<Card.Title>Appointments</Card.Title>
-							<Card.Text>More ways than ever to get the care you need</Card.Text>
-							<button>Appointments & Access</button>
-						</Card.Body>
-					</Card>
-				</CardGroup>
-				<div></div>
+				<Row>
+					<Col md={6}>
+						<Card className="banner-card">
+							<Button className="btn-secondary">Need Appointment?</Button>
+						</Card>
+					</Col>
+					<Col md={6}>
+						<Card className="banner-card">
+							<Button className="btn-secondary">
+								<FcNightPortrait />
+								Search Professionals <FcSearch />
+							</Button>
+						</Card>
+					</Col>
+				</Row>
 			</div>
 		</Container>
 	);
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,9 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
 import React, { useEffect, useState } from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Card, CardGroup, Col, Container, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import img from '../../images/lastpart.jpg';
 import Banner from '../Banner/Banner';
 import Service from '../Service/Service';
 import './Home.css';
@@ -39,6 +40,47 @@ const Home = () => {
 						</button>
 					</Link>
 				</div>
+				<div className="last-part">
+					<Row>
+						<Col md={4}>
+							<Card>
+								<Card.Img variant="top" src={img} />
+							</Card>
+						</Col>
+						<Col md={5}>
+							<h2>Virtual Visits: Expert Care at Your Fingertips</h2>
+							<p>See the doctors and specialists you trust from the comfort of your home. My Healthcare Virtual Visits allow our providers to diagnose and treat a variety of conditions using simple video chat. Contact your My Healthcare provider to request your appointment today!</p>
+							<Link to="/aboutus">
+								Learn More <FontAwesomeIcon icon={faArrowAltCircleRight} />
+							</Link>
+						</Col>
+					</Row>
+				</div>
+				<CardGroup className="advantage">
+					<Card className="advantage-card">
+						<Card.Body>
+							<Card.Title>0ur Doctors</Card.Title>
+
+							<Card.Text>Choose by name, specialty, city and more</Card.Text>
+
+							<button>Find Doctor</button>
+						</Card.Body>
+					</Card>
+					<Card className="advantage-card">
+						<Card.Body>
+							<Card.Title>Locations & Directions</Card.Title>
+							<Card.Text>Find maps and more for all locations</Card.Text>
+							<button>Find a Location</button>
+						</Card.Body>
+					</Card>
+					<Card className="advantage-card">
+						<Card.Body>
+							<Card.Title>Appointments</Card.Title>
+							<Card.Text>More ways than ever to get the care you need</Card.Text>
+							<button>Appointments & Access</button>
+						</Card.Body>
+					</Card>
+				</CardGroup>
 			</Container>
 		</div>
 	);
